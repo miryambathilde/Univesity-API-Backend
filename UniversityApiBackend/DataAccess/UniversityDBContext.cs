7a74ed849b842202e5597c174f642bbccebf93ea
MiryamBathilde <mbathilde@gmail.com>
2022-08-31 14:41:41 +0200

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        {
+
+        }
+
+        // Add DbSets (Tables of our Data Base) 
+        public DbSet<User>? Users { get; set; }
+
+    }
+}
